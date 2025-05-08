ab478002477f761842fd2a5838fcc5da2f076071
vinys2020 <noraelizabethibarra@macbookair.home>
2025-05-07 22:27:40 -0300

    fixurls

--- src/components/HorizontalScroll.jsx
+++ src/components/HorizontalScroll.jsx
@@ -5,17 +5,17 @@ const items = [
     {
       image: "https://http2.mlstatic.com/D_NQ_NP2X_740285-MLA81157648257_122024-O.webp",
       alt: "Protectores solares",
-      link: "https://listado.mercadolibre.com.ar/supermercado/_Container_mk-spm-solares"
+      link: "#"
     },
     {
       image: "https://http2.mlstatic.com/D_NQ_NP2X_835746-MLA81157716337_122024-O.webp",
       alt: "Cuidado del cabello",
-      link: "https://listado.mercadolibre.com.ar/supermercado/cuidado-del-cabello"
+      link: "#"
     },
     {
       image: "https://http2.mlstatic.com/D_NQ_NP2X_869712-MLA81157619195_122024-O.webp",
       alt: "Incontinencia",
-      link: "https://listado.mercadolibre.com.ar/supermercado/incontinencia"
+      link: "#"
     }
 ];
   
@@ -28,7 +28,7 @@ const HorizontalScroll = () => {
           key={index}
           href={item.link}
           className="scroll-card"
-          target="_blank"
+          target=""
           rel="noopener noreferrer"
         >
           <img src={item.image} alt={item.alt} />
--- src/components/HorizontalScroll2.jsx
+++ src/components/HorizontalScroll2.jsx
@@ -5,37 +5,37 @@ const items = [
   {
     image: "https://http2.mlstatic.com/D_Q_NP_2X_695687-MLA81364471168_122024-G.webp",
     alt: "OFERTAS",
-    link: "https://listado.mercadolibre.com.ar/supermercado/_Discount_20-100#c_container_id=not_apply",
+    link: "#",
     title: "OFERTAS"
   },
   {
     image: "https://http2.mlstatic.com/D_Q_NP_2X_702985-MLA81633888259_122024-G.webp",
     alt: "CAFÉ",
-    link: "https://listado.mercadolibre.com.ar/supermercado/_Container_black-friday-capsulas",
+    link: "#",
     title: "CAFÉ"
   },
   {
     image: "https://http2.mlstatic.com/D_Q_NP_2X_646735-MLA81364443724_122024-G.webp",
     alt: "LIMPIEZA",
-    link: "https://listado.mercadolibre.com.ar/supermercado/_Container_descartables-2025",
+    link: "#",
     title: "LIMPIEZA"
   },
   {
     image: "https://http2.mlstatic.com/D_Q_NP_2X_799360-MLA82317787351_022025-G.webp",
     alt: "SALUDABLES",
-    link: "https://listado.mercadolibre.com.ar/supermercado/_Container_mk-saludables25-cpg",
+    link: "#",
     title: "SALUDABLES"
   },
   {
     image: "https://http2.mlstatic.com/D_Q_NP_2X_833202-MLA81364584956_122024-G.webp",
     alt: "LACTEOS",
-    link: "https://listado.mercadolibre.com.ar/supermercado/_Container_mk-leches-maternizadas-cpg",
+    link: "#",
     title: "LACTEOS"
   },
   {
     image: "https://http2.mlstatic.com/D_Q_NP_2X_846703-MLA81564985462_012025-G.webp",
     alt: "MASCOTAS",
-    link: "https://www.mercadolibre.com.ar/c/animales-y-mascotas",
+    link: "#",
     title: "MASCOTAS"
   }
 ];
@@ -48,7 +48,7 @@ const HorizontalScroll2 = () => {
           key={index}
           href={item.link}
           className="scroll-card-two"
-          target="_blank"
+          target=""
           rel="noopener noreferrer"
         >
           <div className="image-container">
